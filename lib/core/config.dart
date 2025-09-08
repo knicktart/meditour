d87a0c63a764291f6aad01ab57da301c14c3daa0
knicktart <knick.tart@gmail.com>
2025-09-08 12:26:03 +0300

    Update config.dart

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -2,5 +2,5 @@ class AppConfig {
   static const String appTitle = 'MediTour';
   static const String apiBase = 'https://api.example.com/api/v1';
   static const bool useMockApi = true;
-  static const String groqApiKey = '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';
+  static const String groqApiKey = 'GROK_KEY_HERE';
 }
